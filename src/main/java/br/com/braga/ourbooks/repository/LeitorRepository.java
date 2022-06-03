64e5604a3db63cf7d1ece984f60e33004c4e5505
heltonbraga <helton.braga@gmail.com>
2022-06-03 09:35:48 -0300

    cleaning merge leftovers

--- src/main/java/br/com/braga/ourbooks/repository/LeitorRepository.java
+++ src/main/java/br/com/braga/ourbooks/repository/LeitorRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.com.braga.ourbooks.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
--- src/main/java/br/com/braga/ourbooks/repository/LivroRepository.java
+++ src/main/java/br/com/braga/ourbooks/repository/LivroRepository.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package br.com.braga.ourbooks.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,16 +9,3 @@ import br.com.braga.ourbooks.model.Livro;
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
 }
-=======
-package br.com.braga.ourbooks.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import br.com.braga.ourbooks.model.Livro;
-
-@Repository
-public interface LivroRepository extends JpaRepository<Livro, Long> {
-
-}
->>>>>>> c7fefe11b7626090533481250118e890fbfef0bd
